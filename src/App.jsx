4d7c2796b015299608a827872009842475d43382
gift56 <efeasiughu@gmail.com>
2022-10-26 15:23:22 +0100

    gotton the url

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
   useEffect(() => {
     const fetchPosts = async () => {
       setLoading(true);
-      const res = await axios.get();
+      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
     };
   }, []);
 
