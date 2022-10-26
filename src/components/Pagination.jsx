c0de5497a1d403db8668af63c702ee43794dbb46
gift56 <efeasiughu@gmail.com>
2022-10-26 15:43:58 +0100

    using for loop

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -2,6 +2,11 @@ import React from "react";
 
 const Pagination = ({ postsPerPage, totalPosts }) => {
   const pageNumbers = [];
+
+  for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+    
+  }
+
   return <div></div>;
 };
 
