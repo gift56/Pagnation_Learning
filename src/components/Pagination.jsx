bf6a87115ed7a84a69fb1e5bf0c1b89bcc1d9c7e
gift56 <efeasiughu@gmail.com>
2022-10-26 15:52:00 +0100

    passing paginate as a props

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const Pagination = ({ postsPerPage, totalPosts }) => {
+const Pagination = ({ postsPerPage, totalPosts, paginate }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
