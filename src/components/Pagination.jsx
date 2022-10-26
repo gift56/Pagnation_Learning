93047560d3fb1b60a7b70b9b3b61811e727edf80
gift56 <efeasiughu@gmail.com>
2022-10-26 15:44:23 +0100

    getting page numbers

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -4,7 +4,7 @@ const Pagination = ({ postsPerPage, totalPosts }) => {
   const pageNumbers = [];
 
   for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
-    
+    pageNumbers.push(i);
   }
 
   return <div></div>;
