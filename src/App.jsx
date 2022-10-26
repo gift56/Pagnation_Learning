f5760b7d29554f01c2974cc2d4e201d24aaa25b4
gift56 <efeasiughu@gmail.com>
2022-10-26 15:18:12 +0100

    initialized states

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 
 const App = () => {
+  const [posts, setPosts] = useState([]);
   return (
     <div className="container">
       <h1>My App</h1>
