88f5124c20a2d87ea2f25f0f464db528420b2144
gift56 <efeasiughu@gmail.com>
2022-10-26 15:31:44 +0100

    added bootstrap clasees

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 
 const Posts = ({ posts, loading }) => {
-    if(loading) return <h2>Loading...</h2>
-    
-  return <div></div>
+  if (loading) return <h2>Loading...</h2>;
+
+  return <ul className="list-group mb-4"></ul>;
 };
 
 export default Posts;
