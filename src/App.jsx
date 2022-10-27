0188ef5ded4932d05f1474679f89afe61132f554
gift56 <efeasiughu@gmail.com>
2022-10-27 09:29:29 +0100

    getting back datas

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,8 @@ const App = () => {
     fetchPosts();
   }, []);
 
+  console.log(posts);
+
   // Getting current post
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
