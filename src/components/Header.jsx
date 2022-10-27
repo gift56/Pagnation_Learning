27e6849ef48847486eb92b02b824da7f0415069e
gift56 <efeasiughu@gmail.com>
2022-10-27 11:33:00 +0100

    added some stuff

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Header = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
+    <nav className="navbar navbar-dark bg-dark sticky-top">
       <div className="container">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand bg-light rounded-3" href="#">
           <img src="logo.png" alt="logo" width="50" height="50" />
         </a>
       </div>
