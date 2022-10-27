059ab7c74d67f44a60d24926f49d3b0b318b65c4
gift56 <efeasiughu@gmail.com>
2022-10-27 11:48:20 +0100

    making new changes

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -17,22 +17,26 @@ const Posts = ({ posts, loading }) => {
     <div className="row my-4 g-4">
       {posts?.map((post, i) => (
         <div className="col-sm-6 col-lg-4 col-xl-3" key={i}>
-          <div className="card">
+          <div className="card bg-dark">
             <img
               src={post?.picture.large}
               alt={post?.name}
               className="card-img-top"
             />
-            <div className="card-body">
+            <div className="card-body text-light">
               <h5 className="card-title">{`${post?.name.title}. ${post?.name.first} ${post?.name.last}`}</h5>
               <p className="card-text">{post?.email}</p>
             </div>
-            <ul className="list-group list-group-flush">
-              <li className="list-group-item">
+            <ul className="list-group list-group-flush border-white">
+              <li className="list-group-item  bg-dark text-light border-white">
                 Country: {post?.location.country}
               </li>
-              <li className="list-group-item">Phone: {post?.cell}</li>
-              <li className="list-group-item">Age: {post?.dob.age}</li>
+              <li className="list-group-item bg-dark text-light border-white">
+                Phone: {post?.cell}
+              </li>
+              <li className="list-group-item bg-dark text-light border-white">
+                Age: {post?.dob.age}
+              </li>
             </ul>
           </div>
         </div>
