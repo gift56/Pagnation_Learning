ba8fadacb00015130ce2e1d94b5e219c1f205a70
gift56 <efeasiughu@gmail.com>
2022-10-27 09:40:59 +0100

    working on users

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,13 +3,7 @@ import React from "react";
 const Posts = ({ posts, loading }) => {
   if (loading) return <h2>Loading...</h2>;
 
-  return (
-    <ul className="list-group mb-4">
-      {posts?.map((post) => (
-        <li className="list-group-item">{post?.cell}</li>
-      ))}
-    </ul>
-  );
+  return <div className="row"></div>;
 };
 
 export default Posts;
