92cba8b42d176fb2626729a4eb75c6dfb486d4a2
gift56 <efeasiughu@gmail.com>
2022-10-27 09:43:45 +0100

    creating card

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -3,7 +3,17 @@ import React from "react";
 const Posts = ({ posts, loading }) => {
   if (loading) return <h2>Loading...</h2>;
 
-  return <div className="row"></div>;
+  return (
+    <div className="row">
+      {posts?.map((post, i) => (
+        <div className="col-sm-6">
+          <div className="card">
+            
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 };
 
 export default Posts;
